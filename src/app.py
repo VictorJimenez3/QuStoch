2b12f83b8097a497e2c226c3a1b409a40f8808d2
Matthew Tujague <matthewtujague@gmail.com>
2025-02-09 02:41:38 -0500

    reformat + refresh

--- src/app.py
+++ src/app.py
@@ -1,17 +1,15 @@
 from flask import Flask, render_template, url_for
 from flask_cors import CORS
+from flask_socketio import SocketIO
+
 
 app = Flask(__name__)
 CORS(app)
+socket = SocketIO(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    socket.run(app=app, debug=True)
--- src/stockData.py
+++ src/stockData.py
@@ -1,9 +1,13 @@
 import yfinance as yf
 import numpy as np
 import pandas as pd
+import random
+
 from datetime import datetime
 
-def get_stock_daily_data(symbol):
+s_and_p_500 = ['MMM','AOS','ABT','ABBV','ACN','ATVI','AYI','ADBE','AAP','AMD','AES','AET','AMG','AFL','A','APD','AKAM','ALK','ALB','ARE','ALXN','ALGN','ALLE','AGN','ADS','LNT','ALL','GOOGL','GOOG','MO','AMZN','AEE','AAL','AEP','AXP','AIG','AMT','AWK','AMP','ABC','AME','AMGN','APH','APC','ADI','ANDV','ANSS','ANTM','AON','APA','AIV','AAPL','AMAT','APTV','ADM','ARNC','AJG','AIZ','T','ADSK','ADP','AZO','AVB','AVY','BHGE','BLL','BAC','BAX','BBT','BDX','BRK.B','BBY','BIIB','BLK','HRB','BA','BWA','BXP','BSX','BHF','BMY','AVGO','BF.B','CHRW','CA','COG','CDNS','CPB','COF','CAH','KMX','CCL','CAT','CBOE','CBG','CBS','CELG','CNC','CNP','CTL','CERN','CF','SCHW','CHTR','CHK','CVX','CMG','CB','CHD','CI','XEC','CINF','CTAS','CSCO','C','CFG','CTXS','CME','CMS','KO','CTSH','CL','CMCSA','CMA','CAG','CXO','COP','ED','STZ','GLW','COST','COTY','CCI','CSRA','CSX','CMI','CVS','DHI','DHR','DRI','DVA','DE','DAL','XRAY','DVN','DLR','DFS','DISCA','DISCK','DISH','DG','DLTR','D','DOV','DWDP','DPS','DTE','DUK','DRE','DXC','ETFC','EMN','ETN','EBAY','ECL','EIX','EW','EA','EMR','ETR','EVHC','EOG','EQT','EFX','EQIX','EQR','ESS','EL','RE','ES','EXC','EXPE','EXPD','ESRX','EXR','XOM','FFIV','FB','FAST','FRT','FDX','FIS','FITB','FE','FISV','FLIR','FLS','FLR','FMC','FL','F','FTV','FBHS','BEN','FCX','GPS','GRMN','IT','GD','GE','GGP','GIS','GM','GPC','GILD','GPN','GS','GT','GWW','HAL','HBI','HOG','HRS','HIG','HAS','HCA','HCP','HP','HSIC','HES','HPE','HLT','HOLX','HD','HON','HRL','HST','HPQ','HUM','HBAN','HII','IDXX','INFO','ITW','ILMN','INCY','IR','INTC','ICE','IBM','IP','IPG','IFF','INTU','ISRG','IVZ','IQV','IRM','JBHT','JEC','SJM','JNJ','JCI','JPM','JNPR','KSU','K','KEY','KMB','KIM','KMI','KLAC','KSS','KHC','KR','LB','LLL','LH','LRCX','LEG','LEN','LUK','LLY','LNC','LKQ','LMT','L','LOW','LYB','MTB','MAC','M','MRO','MPC','MAR','MMC','MLM','MAS','MA','MAT','MKC','MCD','MCK','MDT','MRK','MET','MTD','MGM','KORS','MCHP','MU','MSFT','MAA','MHK','TAP','MDLZ','MON','MNST','MCO','MS','MSI','MYL','NDAQ','NOV','NAVI','NTAP','NFLX','NWL','NFX','NEM','NWSA','NWS','NEE','NLSN','NKE','NI','NBL','JWN','NSC','NTRS','NOC','NCLH','NRG','NUE','NVDA','ORLY','OXY','OMC','OKE','ORCL','PCAR','PKG','PH','PDCO','PAYX','PYPL','PNR','PBCT','PEP','PKI','PRGO','PFE','PCG','PM','PSX','PNW','PXD','PNC','RL','PPG','PPL','PX','PCLN','PFG','PG','PGR','PLD','PRU','PEG','PSA','PHM','PVH','QRVO','QCOM','PWR','DGX','RRC','RJF','RTN','O','RHT','REG','REGN','RF','RSG','RMD','RHI','ROK','COL','ROP','ROST','RCL','SPGI','CRM','SBAC','SCG','SLB','SNI','STX','SEE','SRE','SHW','SIG','SPG','SWKS','SLG','SNA','SO','LUV','SWK','SBUX','STT','SRCL','SYK','STI','SYMC','SYF','SNPS','SYY','TROW','TPR','TGT','TEL','FTI','TXN','TXT','BK','CLX','COO','HSY','MOS','TRV','DIS','TMO','TIF','TWX','TJX','TMK','TSS','TSCO','TDG','TRIP','FOXA','FOX','TSN','USB','UDR','ULTA','UAA','UA','UNP','UAL','UNH','UPS','URI','UTX','UHS','UNM','VFC','VLO','VAR','VTR','VRSN','VRSK','VZ','VRTX','VIAB','V','VNO','VMC','WMT','WBA','WM','WAT','WEC','WFC','HCN','WDC','WU','WRK','WY','WHR','WMB','WLTW','WYN','WYNN','XEL','XRX','XLNX','XL','XYL','YUM','ZBH','ZION','ZTS']
+
+def get_stock_daily_data(symbol: str):
     """
     Fetches daily historical data for a given symbol using yfinance.
     Retrieves approximately one year of data.
@@ -51,7 +55,7 @@ def compute_historical_volatility(df, days=30, annualize=True):
     
     return vol
 
-def compute_beta(stock_df, market_symbol="^GSPC", days=30): 
+def compute_beta(stock_df, market_symbol: str, days=30): 
     """
     Computes the beta of a stock relative to a market index.
     
@@ -133,7 +137,7 @@ def get_stock_info(symbol):
         print("Failed to compute historical volatility.")
     
     # Compute beta relative to the S&P 500.
-    beta = compute_beta(df, market_symbol="^GSPC", days=30)
+    beta = compute_beta(df, market_symbol = symbol, days=30)
     if beta is not None:
         print(f"Beta (based on last 30 trading days): {beta:.2f}")
     else:
@@ -147,18 +151,18 @@ def get_stock_info(symbol):
         "beta": beta
     }
 
-# Example usage:
-if __name__ == "__main__":
-    symbol = "AAPL"
-    symbol1 = "COF"
+# # Example usage:
+# if __name__ == "__main__":
+#     symbol = random.choice(s_and_p_500)
+#     symbol2 = random.choice(s_and_p_500)
 
-    stock_info = get_stock_info(symbol)
-    stock_info1 = get_stock_info(symbol1)
+#     stock_info = get_stock_info(symbol)
+#     stock_info2 = get_stock_info(symbol2)
     
-    if stock_info is not None:
-        print("\nReturned Stock Information for", symbol)
-        print(stock_info)
+#     if stock_info is not None:
+#         print("\nReturned Stock Information for", symbol)
+#         print(stock_info)
     
-    if stock_info1 is not None:
-        print("\nReturned Stock Information for", symbol1)
-        print(stock_info1)
+#     if stock_info2 is not None:
+#         print("\nReturned Stock Information for", symbol2)
+#         print(stock_info2)
